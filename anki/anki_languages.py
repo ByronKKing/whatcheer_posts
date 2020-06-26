99ce93c3ad3a1ecd1fada6af1c3a9bae7c77a032
Byron King <byronkking@gmail.com>
2020-06-26 17:47:01 +0200

    interact with drive

--- anki/anki_languages.py
+++ anki/anki_languages.py
@@ -1,65 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-##Whole process is following
-
-#star translations on mobile or desktop
-#export saved_translations file on occasion
-#run script to create import files and save cleaned file
-#import into ANKI
-#clear duplicates in ANKI
-
-
-# In[ ]:
-
-
-##Script does the following:
-
-#download saved_translations from google drive API
-#remove duplicates
-#create importable ANKI csv files for each language
-#uploads cleaned saved_translations file to google drive
-
-
-# In[ ]:
-
-
-
-
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
-
+from copy import deepcopy
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 
-# In[3]:
+export_path = '/Users/byronking/Desktop/anki_import/'
 
-
+##Authenticate (note: client_secrets.json need to be in the same directory as the script)
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
 drive = GoogleDrive(gauth)
 
 
-# In[8]:
-
-
-# View all folders and file in your Google Drive
+##Collect Saved Translations files from Drive and save file
 fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-
-
-# In[28]:
-
 
 jsonList = []
 for file in fileList:
@@ -67,91 +34,26 @@ for file in fileList:
         print('ID: %s, Date %s' % (file['id'], file['createdDate']))
         jsonList.append({'id': file['id'], 'date': file['createdDate']})
 
-
-# In[29]:
-
-
 jsonList = sorted(jsonList, key=lambda x: x['date'], reverse = True)
-
-
-# In[31]:
-
 
 fileId = jsonList[0]['id']
 
-
-# In[32]:
-
-
 _file = drive.CreateFile({'id': fileId})
-
-
-# In[34]:
-
-
-_file.GetContentFile('/Users/byronking/Desktop/anki_import/translations.xlsx',
+_file.GetContentFile(export_path+'translations.xlsx',
                     mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
 
-# In[ ]:
-
-
-##now upload as clean_translations
-##rid of other save translations files
-
-
-# In[93]:
-
-
-ogdf = pd.read_excel('/Users/byronking/Desktop/anki_import/translations.xlsx', header = None)
-
-
-# In[94]:
-
-
+##Clean files and save Anki upload files
+ogdf = pd.read_excel(export_path+'translations.xlsx', header = None)
 ogdf.columns = ['from_lang', 'to_lang', 'from_trans', 'to_trans']
 
-
-# In[95]:
-
-
-from copy import deepcopy
-
-
-# In[96]:
-
-
 df = deepcopy(ogdf)
-
-
-# In[97]:
-
-
 df.drop_duplicates(['from_trans'], keep= 'first', inplace = True)
 df.drop_duplicates(['to_trans'], keep= 'first', inplace = True)
 df = df[df['to_trans']!=df['from_trans']]
 
-
-# In[98]:
-
-
 all_langs = set(df.from_lang.values).union(set(df.to_lang.values))
-
-
-# In[99]:
-
-
 all_langs.remove('English')
-
-
-# In[100]:
-
-
-export_path = '/Users/byronking/Desktop/anki_import/'
-
-
-# In[104]:
-
 
 for lang in all_langs:
     if lang == 'Russian':
@@ -164,33 +66,16 @@ for lang in all_langs:
                                                header = None,
                                                encoding="utf-8-sig")
 
-
-# In[ ]:
-
+df.to_csv(export_path + "clean_translations.csv", index = False)
 
 
+##Upload to Drive and delete all Saved Translation files
+upload = drive.CreateFile({'title': 'clean_translations.csv'})
+upload.SetContentFile(export_path + "clean_translations.csv")
+upload.Upload()
+print('Uploaded file with ID {}'.format(upload.get('id')))
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+for file in jsonList:
+    removeFile = drive.CreateFile({'id': file['id']})
+    removeFile.Delete()
 
